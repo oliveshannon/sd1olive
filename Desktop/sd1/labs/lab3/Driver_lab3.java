611deb793522f7ca9329ef2a6582fb612c425a05
Olive Shannon <oliveshannon@148-100-177-228.FoxNet.marist.edu>
2017-03-01 18:10:00 -0500

    Added the screenshot of my passing the kattis problem that I had forgotten to add in the original commit

--- Desktop/sd1/labs/lab3/Driver_lab3.java
+++ Desktop/sd1/labs/lab3/Driver_lab3.java
@@ -12,14 +12,9 @@ public class Driver_lab3 {
     double x2 = input.nextDouble();
     double y2= input.nextDouble();
     double p = input.nextDouble();
-    double distance+ CalcDistances(x1, x2, y1, y2, p);
+    double distance = Math.pow(Math.pow(Math.abs(x1-x2), p) 
+      + Math.pow(Math.abs(y1-y2), p), 1.0/p);
     System.out.println(distance);
     }
-  }
-  public static double CalcDistances(double x1, double x2,
-      double y1, double y2, double p) {
-    double distance = Math.pow(math.pow(Math.abs(x1-x2), p) 
-      + Math.pow(Math.abs(y1-y2), p), 1.0/p);
-    return distance;
   }
 }
--- Desktop/sd1/labs/lab3/ex6_3.java
+++ Desktop/sd1/labs/lab3/ex6_3.java
@@ -1,18 +1,26 @@
+/**
+   * file: ex6_3.java
+   * author: Olive Shannon
+   * course: CMPT 220
+   * assignment: lab 3
+   * due date: Feburary 23th, 2017
+   * version: 1.0
+   */
 import java.util.Scanner;
 public class ex6_3 {
   public static void main(String[] args) {
     System.out.print("Enter a number: ");
     Scanner input = new Scanner(System.in);
-    int number = input.nextInt();
-    System.out.print(number+ (isPalindrome(number) ? " is " : " is not ")
+    int num = input.nextInt();
+    System.out.print(number+ (isPalindrome(num) ? " is " : " is not ")
       + "a palindrome.");
   }
-  public static boolean isPalindrome(int number) {
-    return number == reverse(number) ? true : false;
+  public static boolean isPalindrome(int num) {
+    return num == reverse(num) ? true : false;
   }
   public static int reverse(int number) {
     String reverse=""; 
-    String n = number + "";
+    String n = num + "";
     for (int i=n.length()-1; i>=0; i--) {
 // This should reverse the string.
       reverse += n.charAt(i);
