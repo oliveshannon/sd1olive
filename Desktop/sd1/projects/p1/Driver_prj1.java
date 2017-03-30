c7027fa43a6133af56f33bc1a48bf608c1698cb8
Olive Shannon <oliveshannon@148-100-145-122.FoxNet.marist.edu>
2017-03-30 00:19:30 -0400

    version 1.3 of project 1.

---
+++ Desktop/sd1/projects/p1/Driver_prj1.java
@@ -0,0 +1,49 @@
+/**
+   * file: project1.java
+   * author: Olive Shannon
+   * course: CMPT 220
+   * assignment: project 1
+   * due date: March 30th, 2017
+   * version: 1.3
+   */
+import java.util.Scanner;
+public class Driver_pr1 {
+  public static void main(String[] args) {
+    int vectorAsize =0;
+    int vectorBsize=0;
+    Scanner input= new Scanner(System.in);
+    System.out.println("Enter the size of the first vector: ");
+    vectorAsize= input.nextInt();
+    System.out.println("Enter the size of the second vector: ");
+    vectorBsize= input.nextInt();
+    double[] vectorA= new double[vectorAsize];
+    double[] vectorB= new double[vectorBsize];
+    System.out.println("Enter the data for the first vector: ");
+    for(int i=0; i < vectorAsize; i++){
+      vectorA[i]= input.nextInt();
+    } 
+    System.out.println("Enter the data for the first vector: ");
+    for(int i=0; i < vectorBsize; i++){
+      vectorB[i]= input.nextInt();
+    } 
+    double[] finalData= convolute(vectorA, vectorB);
+    for (int i=0; i< finalData.length; i++){
+      finalData[i]= Math.round(finalData[i]);
+    }
+    for (double e: finalData){
+      System.out.print(" " + e);
+    }
+  }
+  public static double[] convolute(double[] vectorA, double[] vectorB){
+    double[] finalData= new double[(vectorA.length- vectorB.length)-1];
+    for (int index=0; index< finalData.length; index++){
+      for (int shift=0; shift< vectorB.length; shift++){
+        finalData[index]= vectorA[index-shift] * vectorB[index];
+        if (finalData[index] < 0 || finalData[index] > finalData.length){
+          finalData[index]= 0; // this is supposed to replace any out of bounds
+        }                      // as a 0 in the result. trying to find more
+      }                        // more optimal solutions.
+    }
+    return finalData;
+  }
+}
